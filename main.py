1a34eb193054782109018c2aedda4edc0567bdc9
Julie Xiao <julixiao@gmail.com>
2021-05-22 23:13:03 -0400

    Added start page and images of cards

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ kivy.require('1.0.6') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput 
+
+class StartPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+
+        self.add_widget(Label(text="Cards: Make 24"))
+        self.answer = TextInput(multiline=False)
+        self.add_widget(self.answer)
 
 class Make24(App):
 
     def build(self):
-        return Label(text='Make 24 Card Game')
+        return StartPage()
 
 
 if __name__ == '__main__':
